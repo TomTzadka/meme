1182e6cf2a67529b73fb443050064cafc48b6489
TomTzadka <tomtzadka@gmail.com>
2024-03-02 20:02:44 +0200

    End of Sprint

--- js/services/util.service.js
+++ js/services/util.service.js
@@ -1,18 +1,8 @@
 "use strict";
 
-
-
-
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
-function _getAsCSV(objects) {
-  let csvStr = `id, name`;
-  objects.forEach((object) => {
-    const csvLine = `\n${object.id}, ${object.name},`;
-    csvStr += csvLine;
-  });
-  return csvStr;
-}
+
 
 
